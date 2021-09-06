547e53c8c8305562d5ccd41d322ce70ef0c43a7d
iMohsinali <87361294+iMohsinali@users.noreply.github.com>
2021-09-06 16:12:27 +0500

    Add menu and social icon
    Add menu and social icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,52 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import React, { useState, useRef, } from 'react'
+import { FaBars, FaTwitter, FaFacebook, FaWhatsapp } from 'react-icons/fa'
+
 import { links, social } from './data'
-import logo from './logo.svg'
+import logo from './logo.png'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [show, setshow] = useState(false)
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img className='imge' src={logo} alt='logo' />
+          <button className='nav-toggle' onClick={setshow(!show)}>
+            <FaBars />
+
+          </button>
+        </div>
+        <div className={`show? 'links-container show-container': 'links-container'`}>
+          <ul className='links'>
+            {
+              links.map((link) => {
+                const { id, url, text } = link
+                return (
+                  <li key={id}>
+                    <a href={url}> {text}</a>
+
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {
+            social.map((social) => {
+              const { id, url, icon } = social;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+
+    </nav>
+  )
 }
 
 export default Navbar
